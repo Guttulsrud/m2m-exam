8db53d7f26849e140daae4e34ac76dc02e7613a3
Håkon Guttulsrud <guttulsrud@outlook.com>
2020-11-08 22:23:05 +0100

    implemented engine/fuel level

--- src/Exam.ino
+++ src/Exam.ino
@@ -1,23 +1,32 @@
+#include "TimeAlarms/TimeAlarms.h"
 #include "lib/mqtt/MQTTHandler.h"
 #include "lib/sim7600/SIM7600Handler.h"
 
 // Handlers
 SIM7600Handler SIM7600;
+MQTTHandler MQTT;
 
+int fuelLevelPercent;
+bool poweredOn = false;
 void setup() {
 
+  pinMode(D2, OUTPUT);
+  pinMode(D1, INPUT);
   Serial.println("Starting...");
   Particle.function("dashboardrequest", handleDashboardRequest);
   Serial.begin(19200);
+  setUpTimers();
 
   SIM7600.init();
 }
 
 void loop() {
 
-  SIM7600.requestCoordinates();
-
-  delay(10000);
+  if (Particle.connected()) {
+    SIM7600.requestCoordinates();
+    delay(20000);
+    Alarm.delay(1);
+  }
 
   if (Serial.available() > 0) {
     Serial.print(">");
@@ -40,4 +49,39 @@ void loop() {
   }
 }
 
-int handleDashboardRequest(String command) { SIM7600.requestCoordinates(); }
+void setUpTimers() { Alarm.timerRepeat(400, measureFuelLevel); }
+
+int handleDashboardRequest(String command) {
+
+  if (command == "1") {
+    powerOnEngine();
+  }
+  if (command == "2") {
+    powerOffEngine();
+  }
+}
+
+void measureFuelLevel() {
+
+  // Generate random fuel level percent 40-90%
+  fuelLevelPercent = random(40, 90);
+
+  if (!digitalRead(D1)) {
+    // Tank is full!
+
+  } else {
+    // Tank is not full
+  }
+
+  MQTT.sendMQTT(String(fuelLevelPercent), "fuel_level");
+}
+
+void powerOnEngine() {
+  poweredOn = true;
+  digitalWrite(D2, HIGH);
+}
+
+void powerOffEngine() {
+  poweredOn = false;
+  digitalWrite(D2, LOW);
+}
